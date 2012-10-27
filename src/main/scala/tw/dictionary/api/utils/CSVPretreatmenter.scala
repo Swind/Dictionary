58b3ab1a401ae99f9e9a746e6b84d0e91157aea3
Swind <idle.swind@gmail.com>
2012-10-27 20:26:27 +0800

    Add GEPTmediumhigh words list.

---
+++ src/main/scala/tw/dictionary/api/utils/CSVPretreatmenter.scala
@@ -0,0 +1,32 @@
+package tw.dictionary.api.parser.utils
+import java.io.BufferedReader
+import java.io.FileWriter
+import java.io.FileReader
+
+object CSVPretreatmenter {
+  def handleLines(srcPath: String, destPath: String)(handler: (String) => String) = {
+    val reader = new BufferedReader(new FileReader(srcPath))
+    val writer = new FileWriter(destPath, false)
+
+    def handleAllLine(reader: BufferedReader, writer: FileWriter, handler: (String) => String): Unit = {
+      val line = reader.readLine()
+      if (line != null) {
+        writer.write(handler(line))
+        handleAllLine(reader, writer, handler)
+      }
+    }
+
+    handleAllLine(reader, writer, handler)
+  }
+
+  def main(args: Array[String]) {
+    handleLines("./data/GEPTmediumhigh.csv", "./data/GEPTmediumhigh.txt") {
+      line =>
+        try {
+          line.split(",").first + "\n"
+        } catch {
+          case _ => ""
+        }
+    }
+  }
+}
